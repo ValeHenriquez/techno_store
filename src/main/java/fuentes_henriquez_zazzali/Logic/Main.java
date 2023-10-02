b2a30365044d38933d0f1363783c7b4c2e7341bd
SebaFuentes <sebastian.fuentes01@alumnos.ucn.cl>
2023-10-02 14:53:55 -0300

    Main advanced

--- src/main/java/fuentes_henriquez_zazzali/Logic/Main.java
+++ src/main/java/fuentes_henriquez_zazzali/Logic/Main.java
@@ -33,17 +33,28 @@ public class Main {
 			Cliente newClient = new Cliente(client_info[0],client_info[1],client_info[2],client_info[3],client_info[4],client_info[5]);
 
 		}else{
+            System.out.println("Ingrese su rut\n");
 			String rut = scanner.nextLine();
-			System.out.println("Ingrese su rut\n");
 			//Cliente currentClient = 
 		}
         System.out.println("¿How do you wish to begin?: (1) Start buying\n (2) Watch customer with top purchases\n (3) Best selling product\n (4) Worst selling product\n (5) Customer who has spent more money on purchases\n (6) Salir del sistema");     
 		opcion = scanner.nextInt();
         switch(opcion){
             case 1:
-            System.out.println("Enter your Rut\n");
-            //DatabaseSAber.getCLientebyRut(rut);
-            //Estos son los productos disponibles
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Choose de product you want to buy:\n");
+            System.out.println("(1) Computer\n (2) Monitor (3) Mouse:");
+            int chose= scanner2.nextInt();
+            switch(chose){
+                case 1:
+                  String[] computers={"(1) Hp Pavilion Gaming:\n id:Hp-1, Brand: HP, type: Slim model: Pavilion Gaming, CPU: Intel core i5"};
+                  System.out.println("These are the computers availables\n (1) Hp Pavilion Gaming:\n id:Hp-1, Brand: HP, type: Slim CPU: Intel core i5,(2) Legion 5 Pro (3) Lenovo ideapad (4) Hp Victus (5) :");
+
+                case 2:
+
+                case 3:
+
+            }
 
             case 2:
             System.out.println("The customer with top purchases:\n ");
